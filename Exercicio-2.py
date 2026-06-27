249eb85244a81e8717cd7ab46cd38c8cb85b5c83
pedrorventurini@gmail.com <pedro.venturini@aluno.senac.rio>
2026-06-26 21:51:09 -0300

    comentário

---
+++ Exercicio-2.py
@@ -0,0 +1,16 @@
+Arroz = 24.90
+Feijao = 8.50
+Macarrao = 4.30
+Molho_de_Tomate = 3.70
+Oleo_de_Soja = 8.90
+
+Arroz = input('Vai comprar arroz? (sim/não): ')
+Feijao = input('Vai comprar Feijão? (siim/não): ')
+Macarrao = input('Vai comprar Macarrao? (sim/não): ')
+Molho_de_Tomate = input('Vai comprar molho de tomate? (sim/não): ')
+Oleo_de_Soja = input('Vai comprar oleo de soja? (sim/não): ')
+
+Total = 0
+
+if Arroz == "sim":
+   Total = Total + Arroz
