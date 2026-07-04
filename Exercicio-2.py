a5d2f4a5b237b454a416e393242061bcd8c128e6
PedroRVenturini <pedrorventurini@gmail.com>
2026-07-03 21:45:10 -0300

    Atualização aula 2

--- Exercicio-2.py
+++ Exercicio-2.py
@@ -1,16 +1,14 @@
-Arroz = 24.90
+'''Arroz = 24.90
 Feijao = 8.50
 Macarrao = 4.30
 Molho_de_Tomate = 3.70
 Oleo_de_Soja = 8.90
 
-Arroz = input('Vai comprar arroz? (sim/não): ')
-Feijao = input('Vai comprar Feijão? (siim/não): ')
-Macarrao = input('Vai comprar Macarrao? (sim/não): ')
-Molho_de_Tomate = input('Vai comprar molho de tomate? (sim/não): ')
-Oleo_de_Soja = input('Vai comprar oleo de soja? (sim/não): ')
+respo_arroz = input('deseja comprar arroz: ')
+respo_feijão = input('deseja comprar feijão: ')
+resp_macarrao = input('deseja compra macarrao: ')
+respo_molho_tomate = input('deseja comprar molho_tomate: ')
+respo_oleo = input('deseja comprar oleo: ')'''
 
-Total = 0
 
-if Arroz == "sim":
-   Total = Total + Arroz
+distancia = float(input('Digite a qtde de kms da viagem: '))
