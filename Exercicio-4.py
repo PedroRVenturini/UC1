57041495c81de456b60e7d1a5ab666963a01cece
PedroRVenturini <pedrorventurini@gmail.com>
2026-07-15 19:19:56 -0300

    Add files via upload

--- Exercicio-4.py
+++ Exercicio-4.py
@@ -1,4 +1,4 @@
-temp = []
+'''temp = []
 
 for i in range(6):
     Temperatura = float(input('Temperatura: '))
@@ -11,7 +11,51 @@ for i in temp:
     elif i >= 15 and i <= 28:
         print('Agradavel')
     else:
-        print('Quente')        
+        print('Quente')'''       
 
 
+def calcular_imc(peso, altura):
+    imc =  peso / (altura**2) 
+    return imc
 
+def tratamento_altura(mensagem):
+    while True:
+        try:
+            altura = int(input(mensagem))
+            print('Altura cadastrada')
+            break
+        except ValueError:
+            print('Digite apenas os Nº sem pontos')
+    return altura
+
+def tratamento_peso():
+    while True:
+        try:
+            peso = float(input('Qual o seu peso? '))
+            print('Peso cadastrado')
+            break
+        except ValueError:
+            print('Digite apenas Nº válidos')
+    return peso
+
+
+alunos = {'nome' : [],
+          'altura': [],
+          'peso': [],
+          'imc' : []}
+
+
+while True:
+    nome = input('Qual seu nome? ')
+    altura = tratamento_altura('Qual a sua altura')
+    peso = tratamento_peso()
+    imc = calcular_imc(peso, altura)
+
+    alunos['nome'].append(nome)
+    alunos['altura'].append(altura)
+    alunos['peso'].append(peso)
+    alunos['imc'].append(imc)
+    break
+
+for i in range(len(alunos['nome'])):
+    print(f'{alunos["nome"][i]}, seu imc é de {round(alunos['imc'][i]*100,2)} %')
